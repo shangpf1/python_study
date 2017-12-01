008ad22263a77e274eb52da122ba59a74fe7da5f
shangpf1 <208418427@qq.com>
2017-12-01 19:16:29 +0800

    练习02

---
+++ 2017-11-28.py
@@ -0,0 +1,77 @@
+"""
+这是我的练习作业
+
+date:2017-11-28
+
+"""
+
+# 算数运算符
+a = 21
+b = 10
+c = 0
+
+c = a + b
+print ("line 1 - value of c is", c )
+
+c = a - b
+print ("line 2 - value of c is", c )
+
+c = a * b
+print ("line 3 - value of c is", c )
+
+c = a / b
+print ("line 4 - value of c is", c )
+
+c = a % b 
+print ("line 5 - value of c is", c )
+
+a = 2
+b = 3
+c = a ** b
+print ("line 6 - value of c is", c )
+
+a = 10 
+b = 5
+c = a // b
+print ("line 7 - value of c is", c )
+
+# 比较预算符
+
+a = 21
+b = 10 
+c = 0
+ 
+if ( a == b ):
+   print ("line 8 - a is equal to b ")
+else:
+    print ("line 8 - a is not equal to b ")
+
+if ( a != b ):
+   print ("line 9 - a is not equal to b ")
+else:
+    print ("line 9 -a is equal to b ")
+
+if ( a < b ):
+   print ("line 10 -a is less than b ")
+else:
+    print ("line 10 - a is not less than b ")
+
+if ( a > b ):
+    print ("line 11 - a is greater than b ")
+else :
+    print ("line 11 - a is not greater than b ")
+
+
+a = 5
+b = 20
+if ( a <= b):
+   print ("line 12 - a is either less than or equal to b ")
+else:
+    print ("line 12 - a is neither less than nor equal to b ")
+
+if ( a >= b):
+   print ("line 13 - a is either greater than or equal to b ")
+else:
+    print ("line 13 - a is neither greater than nor equal to b ")
+
+
