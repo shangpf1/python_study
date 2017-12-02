edce85668c4f5484413bffe90fdf3c04b376a3f3
shangpf1 <208418427@qq.com>
2017-12-02 14:04:37 +0800

    python 赋值运算符

---
+++ 2017-11-29.py
@@ -0,0 +1,28 @@
+"""
+我的练习02-python赋值运算
+
+"""
+
+a = 21  
+b = 10
+c = 3
+c += a  # c = c + a
+print ("line 1 - value of c is",c)
+
+c -=a   # c = c - a
+print ("line 2 - value of c is",c)
+
+c *= a  # c = c * a
+print ("line 3 - value of c is",c)
+
+c /= a  # c = c / a
+print ("line4 - value of c is",c)
+
+c %= a  # c = c % a
+print ("line 5 - value of c is",c)
+
+c //= a  # c = c // a
+print ("line 6 - value of c is",c)
+
+c **= a  # c = c ** a
+print ("line 7 - value of c is",c)
