a618505abb3f97d502515f9550f13ccb2a743167
shangpf1 <208418427@qq.com>
2017-12-18 22:12:52 +0800

    python-homework01

--- 2017-12/2017-12-6-01.py
+++ 2017-12/2017-12-6-01.py
@@ -1,7 +1,6 @@
 '''
 我的练习作业03 python-homework01
 
-
 '''
 
 # 求从10到100中能被3或5整除的数的和
@@ -9,24 +8,27 @@ sum=0
 for x in range(10,101):
    if(x%3==0 or x%5==0):
        sum+=x
-print(sum)
+print(x,sum)
 
 
 
-# #求从1~100的奇数和
-# sum=0
-# i=1
-# while i<101:
-#     sum+=i
-#     i+=1
-# print(sum)
+# #求从1~100的奇数和 (2种方法如下：)
+sum=0
+i=1
+while i<101:
+    sum+=i
+    i+=2
+print("1~100的奇数和：",sum)
+
+
+sum=0
+for x in range(1,101,2):
+    sum+=x
+print(x,sum)
 
 
 # # 求从1~10的所有偶数
-# for y in range(1,11):
-#     if(y%2==0):
-#         print(y)
-        
-
-    
-
+for y in range(1,11):
+    if(y%2==0):
+        print(y)
+print('=======================')
