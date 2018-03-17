702750e9376c9875fa50ad242c6c243ba7e6e90a
shangpf1 <208418427@qq.com>
2018-03-17 23:08:52 +0800

    python接口自动化-requests库发送get/post请求

---
+++ 2018-02/2018-02-26.py
@@ -0,0 +1,21 @@
+
+import requests
+
+# url = "https://api.github.com/user"
+
+
+# # 1. python接口自动化-requests库模拟用户登录
+
+# r = requests.get(url,auth=('shangpf1','spf123456'))
+# print(r.json())
+
+
+#  2. python接口自动化-requests发送post请求
+import json
+url = "https://api.github.com/user/repos"
+post_data = {
+	"name":"helloworld"
+}
+r = requests.post(url,json=post_data,auth=('shangpf1','spf123456'))
+print(r)
+
