29ab5fd09ad2c386f052ce30c2ec48f02d8fb3ee
shangpf1 <208418427@qq.com>
2018-03-17 23:13:19 +0800

    python接口自动化-requests库发送get/post请求

--- 2018-02/2018-02-26.py
+++ 2018-02/2018-02-26.py
@@ -1,18 +1,20 @@
 
 import requests
 
-# url = "https://api.github.com/user"
-
-
 # # 1. python接口自动化-requests库模拟用户登录
+
+# url = "https://api.github.com/user"
 
 # r = requests.get(url,auth=('shangpf1','spf123456'))
 # print(r.json())
 
 
 #  2. python接口自动化-requests发送post请求
+
 import json
+
 url = "https://api.github.com/user/repos"
+
 post_data = {
 	"name":"helloworld"
 }
