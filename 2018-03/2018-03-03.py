5ae15ec77b8481e0510fa66baaf0f90fc73b32bc
shangpf1 <208418427@qq.com>
2018-04-08 21:22:38 +0800

    python 项目日期函数优化-格式化日期、某年某月日历

--- 2018-03/2018-03-03.py
+++ 2018-03/2018-03-03.py
@@ -21,3 +21,11 @@ print(time.strftime("%a %b %d %H:%M:%S %Y"))
 # 将格式字符串转换为时间戳
 a = "Sat Mar 28 22:24:24 2016"
 print(time.mktime(time.strptime(a,"%a %b %d %H:%M:%S %Y")))
+print('-----------------------------------------------')
+
+# 获取某年某月某日的日期
+# Calendar模块有很广泛的方法用来处理年历和月历，例如打印某月的月历
+import calendar
+
+cal = calendar.month(2018,4)
+print("以下输出2016年1月份的日历:",cal)
